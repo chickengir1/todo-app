e7955ae7436f6f06fc54663fa251b55fac2682a3
Lee gangho <rkdgh1198@gmail.com>
2025-04-30 21:02:04 +0900

    refactor: Remove appConfig And improvement App components

--- src/app/features/todo/components/todo-item/todo-item.component.ts
+++ src/app/features/todo/components/todo-item/todo-item.component.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ★ 추가
 import type { Todo } from '../../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
+  standalone: true,
+  imports: [CommonModule], // ★ CommonModule 등록
   templateUrl: './todo-item.component.html',
 })
 export class TodoItemComponent {
   @Input({ required: true }) todo!: Todo;
 
   @Output() toggle = new EventEmitter<string>();
-
   @Output() remove = new EventEmitter<string>();
 }
